e771d9dd30a3ad8c78c353b20bc25a90e98666b3
blackkamenrider <hosias.souza@gmail.com>
2022-11-29 10:13:07 -0300

    Project created

---
+++ src/main/java/com/hosias/evolucao/EvolucaoApplication.java
@@ -0,0 +1,13 @@
+package com.hosias.evolucao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvolucaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvolucaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hosias/evolucao/EvolucaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hosias.evolucao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvolucaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
